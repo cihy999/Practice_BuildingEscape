26195f40514f80c9a5922a40b29f36b6a4b50bde
cihy999 <cihy999@gmail.com>
2022-03-07 13:20:02 +0800

    finish: 107 Trigger Volumes

--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -30,4 +31,7 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float TargetYaw = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
 };
