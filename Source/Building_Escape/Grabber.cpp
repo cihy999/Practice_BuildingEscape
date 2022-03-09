618625842fe7d5a71b80bbd11bc68ff231c7e670
cihy999 <cihy999@gmail.com>
2022-03-09 13:32:34 +0800

    finish: 114 ~ 119 Draw ray-casting debug line

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,50 @@
+// Copyright Cindy C 2022
+
+#include "DrawDebugHelpers.h"
+#include "Grabber.h"
+
+#define OUT // 協助辨識變數是不是Reference類型
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector playerViewPointLocation;
+	FRotator playerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT playerViewPointLocation,
+		OUT playerViewPointRotation
+	);
+
+	FVector lineTraceEnd = playerViewPointLocation + playerViewPointRotation.Vector() * Reach;
+	DrawDebugLine(
+		GetWorld(),
+		playerViewPointLocation,
+		lineTraceEnd,
+		FColor(0, 255, 0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+}
